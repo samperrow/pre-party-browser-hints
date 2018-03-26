423a28e4befb8c71684beb79d4313d14e3bbedad
sam.perrow <sam.perrow399@gmail.com>
2018-03-26 17:41:32 -0500

    add version to admin CSS file to bust cache

--- admin.php
+++ admin.php
@@ -72,8 +72,8 @@ function gktpp_register_admin_files() {
 	global $pagenow;
 
 	if ( isset( $_GET['page'] ) && $_GET['page'] === 'gktpp-plugin-settings' ) {
-		wp_register_script( 'gktpp_admin_js', plugin_dir_url( __FILE__ ) . 'js/admin.js', null, null, false );
-		wp_register_style( 'gktpp_styles_css', plugin_dir_url( __FILE__ ) . 'css/styles.css', null, null, 'all' );
+		wp_register_script( 'gktpp_admin_js', plugin_dir_url( __FILE__ ) . 'js/admin.js', null, '1.5.3', false );
+		wp_register_style( 'gktpp_styles_css', plugin_dir_url( __FILE__ ) . 'css/styles.css', null, '1.5.3', 'all' );
 
 		wp_enqueue_script( 'gktpp_admin_js' );
 		wp_enqueue_style( 'gktpp_styles_css' );
