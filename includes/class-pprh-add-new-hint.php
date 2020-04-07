11ad17a538343006fee480b081a71d5a60548a82
sam perrow <sam.perrow399@gmail.com>
2020-04-06 21:54:54 -0400

    1.7.1

--- includes/class-pprh-add-new-hint.php
+++ includes/class-pprh-add-new-hint.php
@@ -25,7 +25,7 @@ class Add_New_Hint {
 
 				<thead>
 					<tr>
-						<th colspan="5" style="text-align: center; font-size: 23px; font-weight: 600; padding: 15px 0;"><?php esc_html_e( 'Add New Resource Hint', 'pprh' ); ?></th>
+						<th colspan="5" style="text-align: center; font-size: 23px; font-weight: 600; padding: 15px 0;"><?php esc_html_e( 'Add New Resource Hint', 'pre-party-browser-hints' ); ?></th>
 					</tr>
 				</thead>
 
@@ -42,7 +42,7 @@ class Add_New_Hint {
 				<tfoot>
 					<tr>
 						<th style="text-align: center; padding: 20px 0;" colspan="5">
-							<input id="pprhSubmitHints" type="submit" class="button button-primary" value="<?php esc_attr_e( 'Insert Resource Hint', 'pprh' ); ?>" />
+							<input id="pprhSubmitHints" type="submit" class="button button-primary" value="<?php esc_attr_e( 'Insert Resource Hint', 'pre-party-browser-hints' ); ?>" />
 						</th>
 					</tr>
 				</tfoot>
@@ -61,12 +61,12 @@ class Add_New_Hint {
 		<tr>
 			<td colspan="1">
 				<span class="pprh-help-tip-hint">
-					<span><?php esc_html_e( 'Enter a domain name for dns-prefetch and preconnect hints, otherwise enter a full URL.', 'pprh' ); ?></span>
+					<span><?php esc_html_e( 'Enter a domain name for dns-prefetch and preconnect hints, otherwise enter a full URL.', 'pre-party-browser-hints' ); ?></span>
 				</span>
-				<?php esc_html_e( 'Domain or URL:', 'pprh' ); ?>
+				<?php esc_html_e( 'Domain or URL:', 'pre-party-browser-hints' ); ?>
 			</td>
 			<td colspan="4">
-				<input id="pprhURL" placeholder="<?php esc_attr_e( 'Enter valid domain or URL here...', 'pprh' ); ?>" class="widefat" name="url" />
+				<input id="pprhURL" placeholder="<?php esc_attr_e( 'Enter valid domain or URL here...', 'pre-party-browser-hints' ); ?>" class="widefat" name="url" />
 			</td>
 		</tr>
 		<?php
@@ -79,7 +79,7 @@ class Add_New_Hint {
 			<td>
 				<div>
 					<span class="pprh-help-tip-hint">
-						<span><?php esc_html_e( 'Insert domain names from external URL\'s to perform DNS resolution early.', 'pprh' ); ?></span>
+						<span><?php esc_html_e( 'Insert domain names from external URL\'s to perform DNS resolution early.', 'pre-party-browser-hints' ); ?></span>
 					</span>
 					<span><?php esc_html_e( 'DNS-Prefetch' ); ?></span>
 					<input name="hint_type" type="radio" value="dns-prefetch"/>
@@ -89,7 +89,7 @@ class Add_New_Hint {
 			<td>
 				<div>
 					<span class="pprh-help-tip-hint">
-						<span><?php esc_html_e( 'Insert the full URL of a resource that is likely to be needed on a page later.', 'pprh' ); ?></span>
+						<span><?php esc_html_e( 'Insert the full URL of a resource that is likely to be needed on a page later.', 'pre-party-browser-hints' ); ?></span>
 					</span>
 					<span><?php esc_html_e( 'Prefetch' ); ?></span>
 					<input name="hint_type" type="radio" value="prefetch"/>
@@ -99,7 +99,7 @@ class Add_New_Hint {
 			<td>
 				<div>
 					<span class="pprh-help-tip-hint">
-						<span><?php esc_html_e( 'Insert the full URL of a page/post your visitors are likely to navigate towards.', 'pprh' ); ?></span>
+						<span><?php esc_html_e( 'Insert the full URL of a page/post your visitors are likely to navigate towards.', 'pre-party-browser-hints' ); ?></span>
 					</span>
 					<span><?php esc_html_e( 'Prerender' ); ?></span>
 					<input name="hint_type" type="radio" value="prerender"/>
@@ -109,7 +109,7 @@ class Add_New_Hint {
 			<td>
 				<div>
 					<span class="pprh-help-tip-hint">
-						<span><?php esc_html_e( 'Insert domain names from external URL\'s to perform DNS resolution, initial connection, and SSL negotiation ahead of time.', 'pprh' ); ?></span>
+						<span><?php esc_html_e( 'Insert domain names from external URL\'s to perform DNS resolution, initial connection, and SSL negotiation ahead of time.', 'pre-party-browser-hints' ); ?></span>
 					</span>
 					<span><?php esc_html_e( 'Preconnect' ); ?></span>
 					<input name="hint_type" type="radio" value="preconnect"/>
@@ -119,7 +119,7 @@ class Add_New_Hint {
 			<td>
 				<div>
 					<span class="pprh-help-tip-hint">
-						<span><?php esc_html_e( 'Insert the full URL of a resource that is needed on a current page.', 'pprh' ); ?></span>
+						<span><?php esc_html_e( 'Insert the full URL of a resource that is needed on a current page.', 'pre-party-browser-hints' ); ?></span>
 					</span>
 					<span><?php esc_html_e( 'Preload' ); ?></span>
 					<input name="hint_type" type="radio" value="preload"/>
@@ -138,19 +138,19 @@ class Add_New_Hint {
 
 			<td colspan="1">
 				<span class="pprh-help-tip-hint">
-					<span><?php _e( 'For various reasons, font files (and others) need to be loaded with crossorigin. <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#Cross-origin_fetches">Source: Mozilla</a>', 'pprh' ); ?></span>
+					<span><?php _e( 'For various reasons, font files (and others) need to be loaded with crossorigin. <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#Cross-origin_fetches">Source: Mozilla</a>', 'pre-party-browser-hints' ); ?></span>
 				</span>
-				<?php esc_html_e( 'Crossorigin?', 'pprh' ); ?>
+				<?php esc_html_e( 'Crossorigin?', 'pre-party-browser-hints' ); ?>
 				<input value="crossorigin" id="pprhCrossorigin" type="checkbox" class="widefat" name="crossorigin"/>
 			</td>
 
 			<td colspan="2" style="text-align: right; padding-right: 40px;">
 				<span class="pprh-help-tip-hint">
 					<span>
-						<?php _e( "Setting this attribute allows the browser to more accurately: <br/> 1) prioritize resource loading <br/>2) store in browser cache <br/>3) apply the correct headers. <a href='https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#The_basics'>Source: Mozilla</a>", 'pprh' ); ?>
+						<?php _e( "Setting this attribute allows the browser to more accurately: <br/> 1) prioritize resource loading <br/>2) store in browser cache <br/>3) apply the correct headers. <a href='https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#The_basics'>Source: Mozilla</a>", 'pre-party-browser-hints' ); ?>
 					</span>
 				</span>
-				<?php esc_html_e( 'as ', 'pprh' ); ?>
+				<?php esc_html_e( 'as ', 'pre-party-browser-hints' ); ?>
 				<select id="pprhAsAttr" name="as_attr">
 					<option></option>
 					<option value="font">font</option>
@@ -166,9 +166,9 @@ class Add_New_Hint {
 
 			<td colspan="2">
 				<span class="pprh-help-tip-hint">
-					<span><?php _e( '&lt;link&gt; elements can accept a type attribute, which contains the MIME type of the resource the element points to. This is especially useful when preloading resources — the browser will use the type attribute value to work out if it supports that resource, and will only download it if so, ignoring it if not. <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#Including_a_MIME_type">Source: Mozilla</a>. (This attribute will attempt to be added automatically.)', 'pprh' ); ?></span>
+					<span><?php _e( '&lt;link&gt; elements can accept a type attribute, which contains the MIME type of the resource the element points to. This is especially useful when preloading resources — the browser will use the type attribute value to work out if it supports that resource, and will only download it if so, ignoring it if not. <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#Including_a_MIME_type">Source: Mozilla</a>. (This attribute will attempt to be added automatically.)', 'pre-party-browser-hints' ); ?></span>
 				</span>
-				<?php esc_html_e( 'type ', 'pprh' ); ?>
+				<?php esc_html_e( 'type ', 'pre-party-browser-hints' ); ?>
 				<select id="pprhTypeAttr" name="type_attr">
 					<option></option>
 					<option value="font/woff">font/woff</option>
